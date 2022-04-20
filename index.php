7ffea3c5df62497439b6dc7a66ce3c7ba50dfa9c
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-20 19:35:27 +0500

    20/04/2022 19:35:26

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
                     <div class="card">
                         <img src="./images/productPic1.png" class="card-img-top" height="200px" alt="...">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Necless 0</h5>
+                            <h5 class="card-title">Necless_1</h5>
                             <p class="card-text">Price : 34 Rs</p>
                             <button type="submit" name="Add_To_Cart" href="#" class="btn btn-success">Add to Cart</button>
-                            <input type="hidden" name="Item_Name" value="Necless">
+                            <input type="hidden" name="Item_Name" value="Necless_1">
                             <input type="hidden" name="Price" value="34">
                         </div>
                     </div>
@@ -32,11 +32,11 @@
                     <div class="card">
                         <img src="./images/productPic2.png" class="card-img-top" height="200px" alt="...">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Necless 1</h5>
+                            <h5 class="card-title">Necless_2</h5>
                             <p class="card-text">Price : 344 Rs</p>
                             <button type="submit" name="Add_To_Cart" href="#" class="btn btn-success">Add to Cart</button>
-                            <input type="hidden" name="Item_Name" value="Necless">
-                            <input type="hidden" name="Price" value="34">
+                            <input type="hidden" name="Item_Name" value="Necless_2">
+                            <input type="hidden" name="Price" value="344">
                         </div>
                     </div>
                 </form>
@@ -46,11 +46,11 @@
                     <div class="card">
                         <img src="./images/productPic3.png" class="card-img-top" height="200px" alt="...">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Necless 2</h5>
+                            <h5 class="card-title">Necless_3</h5>
                             <p class="card-text">Price : 534 Rs</p>
                             <button type="submit" name="Add_To_Cart" href="#" class="btn btn-success">Add to Cart</button>
-                            <input type="hidden" name="Item_Name" value="Necless">
-                            <input type="hidden" name="Price" value="34">
+                            <input type="hidden" name="Item_Name" value="Necless_3">
+                            <input type="hidden" name="Price" value="534">
                         </div>
                     </div>
                 </form>
@@ -61,11 +61,11 @@
                     <div class="card">
                         <img src="./images/productPic4.png" class="card-img-top" height="200px" alt="...">
                         <div class="card-body text-center">
-                            <h5 class="card-title">Necless 3</h5>
+                            <h5 class="card-title">Necless_4</h5>
                             <p class="card-text">Price : 5434 Rs</p>
                             <button type="submit" name="Add_To_Cart" href="#" class="btn btn-success">Add to Cart</button>
-                            <input type="hidden" name="Item_Name" value="Necless">
-                            <input type="hidden" name="Price" value="34">
+                            <input type="hidden" name="Item_Name" value="Necless_4">
+                            <input type="hidden" name="Price" value="5434">
                         </div>
                     </div>
                 </form>
